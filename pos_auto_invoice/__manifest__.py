bb472a507fb1d1d9c683e74efb94b3e3f5f5ef60
sobhan Hassanzadeh <sobhanhsnzde4579@gmail.com>
2026-02-14 16:01:01 +0330

    update in odoo 19.0

---
+++ pos_auto_invoice/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 Coop IT Easy SC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Point of Sale Automatically Invoice",
+    "summary": "Allow to set POS orders as to-invoice by default",
+    "version": "19.0.1.0.0",
+    "category": "Point of Sale",
+    "website": "https://github.com/OCA/pos",
+    "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        "views/res_config_settings_view.xml",
+    ],
+    "assets": {
+        "point_of_sale._assets_pos": ["pos_auto_invoice/static/src/**/*"],
+    },
+}
